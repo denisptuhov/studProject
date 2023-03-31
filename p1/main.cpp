a2e56f61bf66f29c856375b8242f22958ca51823
denisptuhov <42892743+denisptuhov@users.noreply.github.com>
2023-03-31 16:42:48 +0300

    p1 and p2 added

--- p1/main.cpp
+++ p1/main.cpp
@@ -1,5 +1,5 @@
-#include <cstdio>
 #include <iostream>
+#include <cerrno>
 #include <cstring>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,43 +8,37 @@
 #include <unistd.h>
 
 int main(){
-	char const* name = "/mem_ex.shm";
-	int fd = shm_open(name, O_CREAT | O_RDWR,
-                     (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
+	char const* name = "SharedMemory";
+	int fd = shm_open(name, O_CREAT | O_RDWR, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
 	
-	if (fd <= 0)
-		return 0;
-	
-	int res = fchmod(fd, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
-	if (res != 0){
-		shm_unlink(name);
+	if (fd <= 0){
+		std::cout << std::strerror(errno) << std::endl;
 		return 0;
 	}
 	
-	struct stat fileStat;
-	res = fstat(fd, &fileStat);
+	int res = fchmod(fd, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
 	if (res != 0){
-		shm_unlink(name);
+		std::cout << std::strerror(errno) << std::endl;
 		return 0;
 	}
 	
 	int capacity = 1000;
 	res = ftruncate(fd, capacity);
 	if (res != 0){
-		shm_unlink(name);
+		std::cout << std::strerror(errno) << std::endl;
 		return 0;
 	}
 	
-	auto data = (char*)mmap(NULL, capacity, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	auto data = (unsigned char*)mmap(NULL, capacity, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (data == MAP_FAILED){
-		shm_unlink(name);
+		std::cout << std::strerror(errno) << std::endl;
 		return 0;
 	}
 	
-	char arr[13] = "Hello World!";
+	unsigned char arr[13] = "Hello World!";
 	
 	std::memcpy(data, arr, 13);
-	std::cout << data;
+	std::cout << fd;
 	
-	
+	return 0;
 }
